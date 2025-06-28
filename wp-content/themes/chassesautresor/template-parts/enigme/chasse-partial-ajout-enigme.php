32c2200b3d402249fe4afa70aae7f9a8f73a9806
Laurent Pannetier <lpannetier74@gmail.com>
2025-06-28 08:52:02 +0200

    Améliore l'affichage de la carte d'ajout d'énigme

--- wp-content/themes/chassesautresor/template-parts/enigme/chasse-partial-ajout-enigme.php
+++ wp-content/themes/chassesautresor/template-parts/enigme/chasse-partial-ajout-enigme.php
@@ -11,23 +11,35 @@ $has_enigmes     = $args['has_enigmes'] ?? false;
 $chasse_id       = $args['chasse_id'] ?? null;
 $disabled        = $args['disabled'] ?? true;
 $highlight_pulse = $args['highlight_pulse'] ?? false;
+$use_button      = $args['use_button'] ?? false;
 if (!$chasse_id || get_post_type($chasse_id) !== 'chasse') return;
 
 $ajout_url = esc_url(add_query_arg('chasse_id', $chasse_id, home_url('/creer-enigme/')));
 
 ?>
 
-<a
-  href="<?php echo $ajout_url; ?>"
-  id="carte-ajout-enigme"
-  class="carte-ajout-enigme <?php echo $has_enigmes ? 'etat-suivante' : 'etat-vide'; ?> <?php echo $disabled ? 'disabled' : ''; ?><?php echo $highlight_pulse ? ' pulsation' : ''; ?>"
-  data-post-id="0">
-  <div class="contenu-carte">
-    ➕ <?php echo $has_enigmes ? 'Ajouter une énigme' : 'Créer la première énigme'; ?>
-  </div>
-  <div class="overlay-message">
-    <i class="fa-solid fa-circle-info"></i>
-    <p>Complétez d’abord : titre, image, description</p>
-  </div>
-</a>
+<?php if ($use_button) : ?>
+  <a
+    href="<?php echo $ajout_url; ?>"
+    id="carte-ajout-enigme"
+    data-post-id="0"
+  >
+    <i class="fa-solid fa-circle-plus fa-lg"></i>
+    <span class="screen-reader-text">Ajouter une énigme</span>
+  </a>
+<?php else : ?>
+  <a
+    href="<?php echo $ajout_url; ?>"
+    id="carte-ajout-enigme"
+    class="carte-ajout-enigme <?php echo $has_enigmes ? 'etat-suivante' : 'etat-vide'; ?> <?php echo $disabled ? 'disabled' : ''; ?><?php echo $highlight_pulse ? ' pulsation' : ''; ?>"
+    data-post-id="0">
+    <div class="contenu-carte">
+      ➕ <?php echo $has_enigmes ? 'Ajouter une énigme' : 'Créer la première énigme'; ?>
+    </div>
+    <div class="overlay-message">
+      <i class="fa-solid fa-circle-info"></i>
+      <p>Complétez d’abord : titre, image, description</p>
+    </div>
+  </a>
+<?php endif; ?>
 
