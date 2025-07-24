253092d5060ae93d06ebd581fdb22da91fe76a41
Laurent Pannetier <lpannetier74@gmail.com>
2025-07-24 16:37:57 +0200

    Affichage numéro de page

--- wp-content/themes/chassesautresor/template-parts/enigme/partials/enigme-partial-tentatives.php
+++ wp-content/themes/chassesautresor/template-parts/enigme/partials/enigme-partial-tentatives.php
@@ -59,6 +59,7 @@ $pages = $args['pages'] ?? $pages ?? (int) ceil($total / $par_page);
     <button class="pager-first" aria-label="Première page"><i class="fa-solid fa-angles-left"></i></button>
     <button class="pager-prev" aria-label="Page précédente" style="margin-left:5px;"><i class="fa-solid fa-angle-left"></i></button>
   <?php endif; ?>
+  <span class="pager-info" style="margin:0 8px;"><?= esc_html($page); ?> / <?= esc_html($pages); ?></span>
   <?php if ($page < $pages) : ?>
     <button class="pager-next" aria-label="Page suivante" style="margin-left:10px;"><i class="fa-solid fa-angle-right"></i></button>
     <button class="pager-last" aria-label="Dernière page" style="margin-left:5px;"><i class="fa-solid fa-angles-right"></i></button>
