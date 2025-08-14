75243c7480296d13be2bae5909673118fbf91dd2
Laurent Pannetier <lpannetier74@gmail.com>
2025-08-14 09:11:13 +0200

    Améliore la lisibilité des paginations

--- wp-content/themes/chassesautresor/template-parts/enigme/partials/enigme-partial-participants.php
+++ wp-content/themes/chassesautresor/template-parts/enigme/partials/enigme-partial-participants.php
@@ -61,15 +61,15 @@ if ($orderby === 'tentatives') {
     <?php endforeach; ?>
   </tbody>
 </table>
-<div class="pager" style="margin-top:10px;">
+<div class="pager">
   <?php if ($page > 1) : ?>
     <button class="pager-first" aria-label="Première page"><i class="fa-solid fa-angles-left"></i></button>
-    <button class="pager-prev" aria-label="Page précédente" style="margin-left:5px;"><i class="fa-solid fa-angle-left"></i></button>
+    <button class="pager-prev" aria-label="Page précédente"><i class="fa-solid fa-angle-left"></i></button>
   <?php endif; ?>
-  <span class="pager-info" style="margin:0 8px;"><?= esc_html($page); ?> / <?= esc_html($pages); ?></span>
+  <span class="pager-info"><?= esc_html($page); ?> / <?= esc_html($pages); ?></span>
   <?php if ($page < $pages) : ?>
-    <button class="pager-next" aria-label="Page suivante" style="margin-left:10px;"><i class="fa-solid fa-angle-right"></i></button>
-    <button class="pager-last" aria-label="Dernière page" style="margin-left:5px;"><i class="fa-solid fa-angles-right"></i></button>
+    <button class="pager-next" aria-label="Page suivante"><i class="fa-solid fa-angle-right"></i></button>
+    <button class="pager-last" aria-label="Dernière page"><i class="fa-solid fa-angles-right"></i></button>
   <?php endif; ?>
 </div>
 <?php endif; ?>
