a21e360490ca2a125f414e576c53bc386599977a
Laurent Pannetier <lpannetier74@gmail.com>
2025-08-19 15:46:51 +0200

    Remove edit button for puzzle visual text

--- wp-content/themes/chassesautresor/assets/js/core/resume.js
+++ wp-content/themes/chassesautresor/assets/js/core/resume.js
@@ -361,7 +361,7 @@ function mettreAJourLigneResume(ligne, champ, estRempli, type) {
 
   // Ajouter bouton édition ✏️ si besoin
   const dejaBouton = ligne.querySelector('.champ-modifier');
-  const pasDEdition = ligne.dataset.noEdit !== undefined;
+  const pasDEdition = ligne.dataset.noEdit !== undefined || champ === 'enigme_visuel_texte';
 
   if (pasDEdition) {
     ligne.style.cursor = '';
