7532e08d55aa568eae47b85cc254f4f360f06534
Laurent Pannetier <lpannetier74@gmail.com>
2025-08-21 09:43:36 +0200

    Conditionally load topbar script on enigma pages

--- wp-content/themes/chassesautresor/functions.php
+++ wp-content/themes/chassesautresor/functions.php
@@ -301,13 +301,15 @@ add_action('wp_enqueue_scripts', function () {
             filemtime($theme_path . '/assets/js/enigme-panel.js'),
             true
         );
-        wp_enqueue_script(
-            'enigme-topbar',
-            $script_dir . 'enigme-topbar.js',
-            [],
-            filemtime($theme_path . '/assets/js/enigme-topbar.js'),
-            true
-        );
+        if (!wp_is_mobile()) {
+            wp_enqueue_script(
+                'enigme-topbar',
+                $script_dir . 'enigme-topbar.js',
+                [],
+                filemtime($theme_path . '/assets/js/enigme-topbar.js'),
+                true
+            );
+        }
     }
 });
 
