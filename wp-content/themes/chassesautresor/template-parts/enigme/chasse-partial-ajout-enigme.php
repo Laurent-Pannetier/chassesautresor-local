abcad59575b280e9606cea58dd70b94e965f7124
Laurent Pannetier <lpannetier74@gmail.com>
2025-08-29 16:36:00 +0200

    Refondre la carte d'ajout d'énigme

--- wp-content/themes/chassesautresor/template-parts/enigme/chasse-partial-ajout-enigme.php
+++ wp-content/themes/chassesautresor/template-parts/enigme/chasse-partial-ajout-enigme.php
@@ -11,43 +11,29 @@ $has_enigmes     = $args['has_enigmes'] ?? false;
 $chasse_id       = $args['chasse_id'] ?? null;
 $disabled        = $args['disabled'] ?? true;
 $highlight_pulse = $args['highlight_pulse'] ?? false;
-$use_button      = $args['use_button'] ?? false;
+
 if (!$chasse_id || get_post_type($chasse_id) !== 'chasse') return;
 
 $ajout_url = esc_url(add_query_arg('chasse_id', $chasse_id, home_url('/creer-enigme/')));
 
 ?>
 
-<?php if ($use_button) : ?>
-  <a
-    href="<?php echo $ajout_url; ?>"
+<button
+    type="button"
     id="carte-ajout-enigme"
+    class="carte carte-enigme carte-ajout-enigme <?php echo $has_enigmes ? 'etat-suivante' : 'etat-vide'; ?> <?php echo $disabled ? 'disabled' : ''; ?><?php echo $highlight_pulse ? ' pulsation' : ''; ?>"
     data-post-id="0"
-  >
-    <i class="fa-solid fa-circle-plus fa-lg" aria-hidden="true"></i>
-    <span><?php echo esc_html__('Ajouter une énigme', 'chassesautresor-com'); ?></span>
-  </a>
-<?php else : ?>
-    <a
-        href="<?php echo $ajout_url; ?>"
-        id="carte-ajout-enigme"
-        class="carte carte-enigme carte-ajout-enigme <?php echo $has_enigmes ? 'etat-suivante' : 'etat-vide'; ?> <?php echo $disabled ? 'disabled' : ''; ?><?php echo $highlight_pulse ? ' pulsation' : ''; ?>"
-        data-post-id="0">
-        <div class="carte-core">
-            <div class="carte-enigme-image">
-                <div class="carte-enigme-cta">
-                    <span class="bouton-cta bouton-cta--color">
-                        <?php echo $has_enigmes
-                            ? esc_html__('Ajouter une énigme', 'chassesautresor-com')
-                            : esc_html__('Créer la première énigme', 'chassesautresor-com'); ?>
-                    </span>
-                </div>
-            </div>
-        </div>
-        <div class="overlay-message">
-            <i class="fa-solid fa-circle-info"></i>
-            <p><?php echo esc_html__('Complétez d’abord : titre, image, description', 'chassesautresor-com'); ?></p>
-        </div>
-    </a>
-<?php endif; ?>
+    aria-label="<?php echo esc_attr__('Ajouter une énigme', 'chassesautresor-com'); ?>"
+    <?php echo $disabled ? 'disabled' : ''; ?>
+    onclick="if(!this.hasAttribute('disabled')){window.location.href='<?php echo $ajout_url; ?>';}"
+>
+    <div class="carte-core">
+        <i class="fa-solid fa-circle-plus" aria-hidden="true"></i>
+        <span class="carte-ajout-libelle"><?php echo esc_html__('Ajouter une énigme', 'chassesautresor-com'); ?></span>
+    </div>
+    <div class="overlay-message">
+        <i class="fa-solid fa-circle-info"></i>
+        <p><?php echo esc_html__('Complétez d’abord : titre, image, description', 'chassesautresor-com'); ?></p>
+    </div>
+</button>
 
