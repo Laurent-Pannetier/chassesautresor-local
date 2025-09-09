0177cd062d5fe5081b1cde3513e221314edbf503
Laurent Pannetier <lpannetier74@gmail.com>
2025-09-09 05:12:45 +0200

    Cast query var to int when loading riddle image

--- wp-content/themes/chassesautresor/inc/access-functions.php
+++ wp-content/themes/chassesautresor/inc/access-functions.php
@@ -1235,15 +1235,9 @@ add_action('init', function () {
  * Le handler effectue les vérifications d’accès, puis sert le fichier s’il est autorisé.
  */
 add_action('template_redirect', function () {
-    $flag = get_query_var('voir_image_enigme');
-    error_log(
-        'template_redirect voir_image_enigme=' . var_export($flag, true) .
-        ' | URI=' . $_SERVER['REQUEST_URI']
-    );
-    if ((int) $flag !== 1) {
+    if ((int) get_query_var('voir_image_enigme') !== 1) {
         return;
     }
-    error_log('[template_redirect] inclusion du handler voir-image-enigme');
 
     $handler = get_stylesheet_directory() . '/inc/handlers/voir-image-enigme.php';
 
