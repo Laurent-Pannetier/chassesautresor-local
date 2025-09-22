61f534c196f4611c6b24ad47a1191290c881905a
Laurent Pannetier <lpannetier74@gmail.com>
2025-09-22 10:57:04 +0200

    Remplacer l’emoji de la région dans le CTA

--- wp-content/themes/chassesautresor/template-parts/chasse/chasse-affichage-complet.php
+++ wp-content/themes/chassesautresor/template-parts/chasse/chasse-affichage-complet.php
@@ -453,7 +453,7 @@ if ($edition_active && !$est_complet) {
               $region_main_link = $region_main['link'] ?? '';
               ?>
               <div class="caracteristique caracteristique-region">
-                <span class="caracteristique-icone" aria-hidden="true">🧭</span>
+                <span class="caracteristique-icone" aria-hidden="true">📍</span>
                 <span class="caracteristique-label"><?= esc_html__('Région', 'chassesautresor-com'); ?></span>
                 <span class="caracteristique-valeur">
                   <?php if ($region_main_link !== '') : ?>
